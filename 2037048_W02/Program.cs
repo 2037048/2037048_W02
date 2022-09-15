bb9fac435ba64d97ffd75502fcebe350dd81e3ac
HAMZA A KHADAWARDI <2037048@stu.kau.edu.sa>
2022-09-15 14:19:31 +0300

    WEEK3C

--- 2037048_W02/Program.cs
+++ 2037048_W02/Program.cs
@@ -10,7 +10,74 @@ namespace _2037048_W02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HI");
+            /*  
+            int a;
+            int b;
+            int c;
+            
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+
+            int x = ((a - b) / ((3 * c)*(a + c)));
+            Console.WriteLine(x); 
+            */
+            /*
+            float f_no = 0.0f;
+            double d_no = 0.0d;
+            decimal m_no = 0.0m;
+
+            f_no = 1 / 3f;
+            d_no = 1 / 3d;
+            m_no = 1 / 3m;
+
+            Console.WriteLine("float is {0}",f_no);
+            Console.WriteLine("double is {0}", d_no);
+            Console.WriteLine("decimal is {0}", m_no);
+            */
+
+            /*
+            double a;
+            double b;
+            double c;
+
+            decimal x1 = -b + (Math.Sqrt(Math.Pow(b, 2) * -4 * a * c) / (2 * a);
+            decimal x2 = -b - (Math.Sqrt(Math.Pow(b, 2) * -4 * a * c) / (2 * a);
+            Console.WriteLine("the quadratic equation is {0} and {1}", x1, x2);
+            */
+
+            /*
+            Console.WriteLine("inter two numbers pls .") ;
+            int BiggerNum = Convert.ToInt32(Console.ReadLine());
+            int SmallerrNum = Convert.ToInt32(Console.ReadLine());
+
+            if (SmallerrNum > BiggerNum) 
+            {
+                BiggerNum = SmallerrNum;
+            }
+
+            Console.WriteLine("The grater number is: {0}",BiggerNum);
+            */
+
+            /*
+
+            string s = Console.ReadLine();
+            int number = Convert.ToInt32(s);
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The even number .");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd .");
+            }
+            */
+
+            int ch = Convert.ToInt32(Console.ReadLine());
+
+            if (ch == 'A' || ch == 'a')
         }
     }
 }
---
+++ week4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f_no = 0.0f;
+            double d_no = 0.0d;
+            decimal m_no = 0.0m;
+
+            f_no = 1 / 3f;
+            d_no = 1 / 3d;
+            m_no = 1 / 3m;
+
+            Console.WriteLine("float is {0}", f_no);
+            Console.WriteLine("double is {0}", d_no);
+            Console.WriteLine("decimal is {0}", m_no);
+
+        }
+    }
+}
